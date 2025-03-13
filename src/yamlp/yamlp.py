0959f67c41818030828ed6eb6c3b2eb53a224337
Oscar Beijbom <oscar.beijbom@gmail.com>
2025-03-13 12:01:09 -0700

    Organizing repo code; setting up system for running unittest; adding a PUT box endpoint

---
+++ src/yamlp/yamlp.py
@@ -0,0 +1,25 @@
+import modal
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from yamlp.webapp import web_app
+
+app = modal.App(name="yamlp")
+volume = modal.Volume.from_name("YAMLP", create_if_missing=True)
+
+
+modal_image = (
+    modal.Image.debian_slim()
+    .apt_install(["libgl1-mesa-glx", "libglib2.0-0"])
+    .pip_install_from_pyproject("pyproject.toml")
+    .add_local_python_source("yamlp")  # I'm getting an "automounting" warning without this. Not sure why.
+    .add_local_dir("src/yamlp", remote_path="/src/yamlp")
+    .add_local_dir("static", remote_path="/static")
+)
+
+
+@app.function(image=modal_image, container_idle_timeout=60, volumes={"/data": volume})
+@modal.asgi_app()
+def index() -> FastAPI:
+    web_app.mount("/images", StaticFiles(directory="/data/images"), name="images")
+    return web_app
