a76af55b0108f6d9ea893b9b027c820695ecbc8c
Oscar Beijbom <oscar.beijbom@gmail.com>
2025-03-16 22:01:18 -0700

    label names shows on image

--- src/yamlp/fixtures.py
+++ src/yamlp/fixtures.py
@@ -6,7 +6,7 @@ from PIL import Image as PILImage
 from sqlmodel import Session, create_engine
 
 from yamlp.config import image_dir, image_url_prefix, sqlite_url
-from yamlp.datamodel import BoundingBox, ObjectDetectionSample
+from yamlp.datamodel import BoundingBox, Label, ObjectDetectionSample
 
 
 def populate_db() -> None:
@@ -33,13 +33,18 @@ def populate_db() -> None:
         session.add_all([sample1, sample2])
         session.commit()
 
+        label1 = Label(name="dog", color="#00A020")
+        label2 = Label(name="cat", color="#FF0000")
+        session.add_all([label1, label2])
+        session.commit()
+
         # Create and add boxes in the same session after images are committed
         box1 = BoundingBox(
             center_x=0.1,
             center_y=0.1,
             width=0.1,
             height=0.1,
-            label_name="dog",
+            label_id=label1.id,
             sample_id=sample1.id,
             annotator_name="alice",
         )
@@ -48,7 +53,7 @@ def populate_db() -> None:
             center_y=0.5,
             width=0.1,
             height=0.1,
-            label_name="dog",
+            label_id=label2.id,
             sample_id=sample1.id,
             annotator_name="bob",
         )
@@ -57,7 +62,7 @@ def populate_db() -> None:
             center_y=0.8,
             width=0.1,
             height=0.1,
-            label_name="dog",
+            label_id=label1.id,
             sample_id=sample2.id,
             annotator_name="alice",
             created_at=datetime.now() - timedelta(days=10),
@@ -70,7 +75,7 @@ def populate_db() -> None:
             center_y=0.8,
             width=0.2,
             height=0.1,
-            label_name="dog",
+            label_id=label1.id,
             sample_id=sample2.id,
             annotator_name="alice",
             previous_box_id=box3.id,
