75585c1a44c109170aa7b55f0f58626ff9b9574f
Oscar Beijbom <oscar.beijbom@gmail.com>
2025-03-16 22:07:16 -0700

    adding a labels page

--- src/yamlp/client/__init__.py
+++ src/yamlp/client/__init__.py
@@ -1,7 +1,9 @@
+from .render_labels import render_label_list_page
 from .render_samples import render_sample_history, render_sample_list_page, render_sample_page
 
 __all__ = [
     "render_sample_list_page",
     "render_sample_page",
     "render_sample_history",
+    "render_label_list_page",
 ]
