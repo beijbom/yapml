7bbb3e9df0c806abbc2f972bc4ea5f6c07f6ee1b
Oscar Beijbom <oscar.beijbom@gmail.com>
2025-03-20 11:52:24 -0700

    moving API routes to seperarate files by table

--- src/yapml/server/webapp.py
+++ src/yapml/server/webapp.py
@@ -4,32 +4,19 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from sqlmodel import SQLModel
+
 from yapml.db import engine
 from yapml.fixtures import populate_db
-from yapml.server.api_routes import router as api_router
+from yapml.server.api import admin_router, boundingbox_router, label_router, sample_router
 from yapml.server.ui_routes import router as ui_router
 
 web_app = FastAPI()
 
-
-@web_app.post("/api/reset-db")
-async def reset_db() -> JSONResponse:
-    try:
-        # Drop all tables
-        SQLModel.metadata.drop_all(engine)
-
-        # Create new tables
-        SQLModel.metadata.create_all(engine)
-        populate_db()
-
-        return JSONResponse({"status": "success", "message": "Database was reset successfully"})
-    except Exception as e:
-        return JSONResponse(status_code=500, content={"status": "error", "message": str(e)})
-
-
-# This should be at app initialization, not in the reset_db function
 os.makedirs("/data/images", exist_ok=True)
 web_app.mount("/images", StaticFiles(directory="/data/images"), name="images")
 
-web_app.include_router(api_router)
+web_app.include_router(admin_router)
+web_app.include_router(boundingbox_router)
+web_app.include_router(label_router)
+web_app.include_router(sample_router)
 web_app.include_router(ui_router)
